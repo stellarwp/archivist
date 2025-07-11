ee00abf062c95f67f341bcea1ac565cd007c4a37
Gustavo Bordoni <bordoni.dev@gmail.com>
2025-07-10 21:17:34 -0400

    fix: rename lint script to type-check and fix TypeScript errors
    - Rename 'lint' script to 'type-check' for clarity
    - Fix TypeScript errors in mock-server.ts by adding non-null assertions
    - All regex match groups are guaranteed to exist due to route matching

--- tests/helpers/mock-server.ts
+++ tests/helpers/mock-server.ts
@@ -19,11 +19,11 @@ export async function startMockServer(): Promise<MockServerResult> {
     // Explorer strategy pages
     { pattern: /^\/link-page$/, handler: () => explorer.linkPage() },
     { pattern: /^\/categories$/, handler: () => explorer.categoriesPage() },
-    { pattern: /^\/category\/(.+)$/, handler: (match) => explorer.categoryPage(match[1]) },
+    { pattern: /^\/category\/(.+)$/, handler: (match) => explorer.categoryPage(match[1]!) },
 
     // Pagination strategy pages
     { pattern: /^\/posts$/, handler: (_, params) => pagination.postsWithQueryParam(params.get('page') || '0') },
-    { pattern: /^\/blog\/page\/(\d+)$/, handler: (match) => pagination.blogWithNextLink(parseInt(match[1])) },
+    { pattern: /^\/blog\/page\/(\d+)$/, handler: (match) => pagination.blogWithNextLink(parseInt(match[1]!)) },
     { pattern: /^\/archive$/, handler: (_, params) => pagination.archiveWithParam(params.get('p') || '0') },
     { pattern: /^\/shop\/electronics$/, handler: (_, params) => pagination.shopElectronics(params) },
     { pattern: /^\/docs\/guides$/, handler: (_, params) => pagination.docsGuides(params) },
@@ -31,21 +31,21 @@ export async function startMockServer(): Promise<MockServerResult> {
     { pattern: /^\/gallery\/photos$/, handler: (_, params) => pagination.galleryPhotos(params) },
     { 
       pattern: /^\/blog\/(\d{4})\/(\d{2})(\/page\/(\d+))?$/, 
-      handler: (match) => pagination.blogArchiveByDate(match[1], match[2], match[4])
+      handler: (match) => pagination.blogArchiveByDate(match[1]!, match[2]!, match[4])
     },
 
     // Content pages
-    { pattern: /^\/article\/(\d+)$/, handler: (match) => content.articlePage(match[1]) },
-    { pattern: /^\/post\/(\d+)$/, handler: (match) => content.postPage(match[1]) },
-    { pattern: /^\/blog\/post\/(\d+)$/, handler: (match) => content.blogPostPage(match[1]) },
-    { pattern: /^\/archive\/post\/(\d+)$/, handler: (match) => content.archivePostPage(match[1]) },
-    { pattern: /^\/product\/(\d+)$/, handler: (match) => content.productPage(match[1]) },
-    { pattern: /^\/photo\/(\d+)$/, handler: (match) => content.photoPage(match[1]) },
-    { pattern: /^\/news\/article\/(\d+)$/, handler: (match) => content.newsArticlePage(match[1]) },
-    { pattern: /^\/docs\/guide\/(\d+)$/, handler: (match) => content.docsGuidePage(match[1]) },
+    { pattern: /^\/article\/(\d+)$/, handler: (match) => content.articlePage(match[1]!) },
+    { pattern: /^\/post\/(\d+)$/, handler: (match) => content.postPage(match[1]!) },
+    { pattern: /^\/blog\/post\/(\d+)$/, handler: (match) => content.blogPostPage(match[1]!) },
+    { pattern: /^\/archive\/post\/(\d+)$/, handler: (match) => content.archivePostPage(match[1]!) },
+    { pattern: /^\/product\/(\d+)$/, handler: (match) => content.productPage(match[1]!) },
+    { pattern: /^\/photo\/(\d+)$/, handler: (match) => content.photoPage(match[1]!) },
+    { pattern: /^\/news\/article\/(\d+)$/, handler: (match) => content.newsArticlePage(match[1]!) },
+    { pattern: /^\/docs\/guide\/(\d+)$/, handler: (match) => content.docsGuidePage(match[1]!) },
     { 
       pattern: /^\/blog\/(\d{4})\/(\d{2})\/post\/(\d+)$/, 
-      handler: (match) => content.blogDatePostPage(match[1], match[2], match[3])
+      handler: (match) => content.blogDatePostPage(match[1]!, match[2]!, match[3]!)
     },
 
     // Static pages
